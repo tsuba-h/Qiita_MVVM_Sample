2580599668de6746174cde2ccf59e8a18fcfd5cd
服部翼 <tsuba-h@arsaga.jp>
2019-10-31 15:37:59 +0900

    Initial Commit

---
+++ Qiita_MVVM_SampleTests/Qiita_MVVM_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Qiita_MVVM_SampleTests.swift
+//  Qiita_MVVM_SampleTests
+//
+//  Created by 服部　翼 on 2019/10/31.
+//  Copyright © 2019 服部　翼. All rights reserved.
+//
+
+import XCTest
+@testable import Qiita_MVVM_Sample
+
+class Qiita_MVVM_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
